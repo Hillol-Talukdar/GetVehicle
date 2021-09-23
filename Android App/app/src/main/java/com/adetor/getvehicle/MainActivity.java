9954ba9d529fabf55c134074fdba03582400a227
“Raiyan13” <tahminkabir13@gmail.com>
2021-09-23 19:31:59 +0600

    add navigation drawer

--- Android App/app/src/main/java/com/adetor/getvehicle/MainActivity.java
+++ Android App/app/src/main/java/com/adetor/getvehicle/MainActivity.java
@@ -1,16 +1,28 @@
 package com.adetor.getvehicle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle actionBarNavigationDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpNavigationDrawer();
+
     }
 
     @Override
@@ -18,5 +30,42 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarNavigationDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.item_1:
+                Toast.makeText(this, "Item 1 is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_2:
+                Toast.makeText(this, "Item 2 is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_3:
+                Toast.makeText(this, "Item 3 is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setUpNavigationDrawer() {
+        DrawerLayout navigationDrawerLayout;
+
+        navigationDrawerLayout = findViewById(R.id.navigation_drawer_layout);
+        actionBarNavigationDrawerToggle = new ActionBarDrawerToggle(this, navigationDrawerLayout, R.string.nav_open, R.string.nav_close);
+
+        navigationDrawerLayout.addDrawerListener(actionBarNavigationDrawerToggle);
+
+        actionBarNavigationDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
