32fcb43700ad7295550286aa50be721d6846a214
Hillol Talukdar <talukdarhillol@gmail.com>
2021-11-30 13:37:20 +0600

    add error handler

--- Website/server/app.js
+++ Website/server/app.js
@@ -3,6 +3,7 @@ const middlewares = require('./middlewares');
 const db = require('./database');
 const router = require('./routes');
 const globalErrorHandler = require('./controllers/handlers/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -13,6 +14,10 @@ app.use(...middlewares);
 db.makeDb();
 
 router.registerApplicationRoutes(app);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
 
 app.use(globalErrorHandler);
 
