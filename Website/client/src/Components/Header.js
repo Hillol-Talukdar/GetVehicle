cb37eb93647071d12e06ac30c8a0b2b31a8e7ce6
DESKTOP-3LORMKK\Raiyan <tahminkabir13@gmail.com>
2022-08-04 23:44:07 +0600

    #46: Complete basic React App Setup

---
+++ Website/client/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export default function Header() {
+  return (
+    <>
+        <div>This is header</div>
+    </>
+  )
+}
---
+++ Website/client/src/Constants/CommonConstants.js
@@ -0,0 +1,1 @@
+//common constants should be here
