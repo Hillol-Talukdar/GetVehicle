75d657839e2ae3ea2e89d49a4ce547e55e5d08c5
Hillol Talukdar <talukdarhillol@gmail.com>
2022-08-28 01:46:20 +0600

    Add redux store, reducer, and vehicle constants.

--- Website/client/src/Components/ContainerComponents/ItemDetailsContainer.js
+++ Website/client/src/Components/ContainerComponents/ItemDetailsContainer.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 
+
 const ItemDetailsContainer = (props) => {
 
     return (
---
+++ Website/client/src/reducers/vehicleReducers.js
@@ -0,0 +1,18 @@
+import {
+  VEHICLE_DETAILS_REQUEST,
+  VEHICLE_DETAILS_SUCCESS,
+  VEHICLE_DETAILS_FAIL,
+} from '../Constants/vehicleConstants';
+
+export const vehicleDetailsReducer = (state = { vehicle: {} }, action) => {
+  switch (action.type) {
+    case VEHICLE_DETAILS_REQUEST:
+      return { laoding: true };
+    case VEHICLE_DETAILS_SUCCESS:
+      return { loading: false, vehicle: action.payload };
+    case VEHICLE_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
