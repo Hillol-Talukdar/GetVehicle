f7c034597c2901bfc22ad2888f421e68ed41a20f
Hillol Talukdar <talukdarhillol@gmail.com>
2022-09-05 02:55:22 +0600

    Add card, caousel, Tabs in vehicle details page

--- Website/client/src/Components/ContainerComponents/ItemDetailsContainer.js
+++ Website/client/src/Components/ContainerComponents/ItemDetailsContainer.js
@@ -1,7 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { Row, Col, Image, Container } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Image,
+  Container,
+  Tabs,
+  Tab,
+  Card,
+  ListGroup,
+} from 'react-bootstrap';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { getVehicleDetails } from '../../Services/VehicleDataService';
 import { ColorConstants } from '../../Constants/CommonConstants';
 
@@ -11,6 +22,8 @@ const ItemDetailsContainer = () => {
 
   const vehicleDetails = useSelector((state) => state.vehicleDetailsReducer);
   const { loading, error, vehicle } = vehicleDetails;
+  let { data } = vehicle;
+  let coverImages = ['/templateBike.jpg', '/templateCar.jpg'];
 
   useEffect(() => {
     dispatch(getVehicleDetails(id));
@@ -28,32 +41,76 @@ const ItemDetailsContainer = () => {
       ) : (
         <>
           <Row className="p-3">
-            <Col md={4}>
-              <Image
-                src={
-                  vehicle?.data?.categories?.category == 'Bike'
-                    ? '/templateBike.jpg'
-                    : '/templateCar.jpg'
-                }
-                alt="Card image cap"
-                fluid
-              />
+            <Col md={6}>
+              {/* {data?.photo && data?.photo.length ? (
+                <Carousel showArrows={true} autoPlay infiniteLoop>
+                  {data?.photo &&
+                    data?.photo.map((photo) => (
+                      <img src={photo.url} key={photo.public_id} />
+                    ))}
+                </Carousel>
+              ) : (
+                <Image
+                  src={
+                    data?.categories?.category == 'Bike'
+                      ? '/templateBike.jpg'
+                      : '/templateCar.jpg'
+                  }
+                  alt="Card image cap"
+                  fluid
+                />
+              )} */}
+
+              <Carousel showArrows={true} autoPlay infiniteLoop>
+                {coverImages.map((photo) => (
+                  <Image src={photo} key={photo.public_id} />
+                ))}
+              </Carousel>
+
+              <Tabs
+                defaultActiveKey="profile"
+                id="uncontrolled-tab-example"
+                className="mb-3"
+              >
+                <Tab eventKey="description" title="Description">
+                  Description
+                </Tab>
+                <Tab eventKey="moree" title="More">
+                  More
+                </Tab>
+              </Tabs>
             </Col>
 
-            <Col md={3}>
-              <p>Model: {vehicle?.data?.model}</p>
-              <p>Transmission: {vehicle?.data?.transmission}</p>
-              <p>Fuel Type: {vehicle?.data?.fuelType}</p>
-              <p>Engine: {vehicle?.data?.engine}</p>
-              <p>Boot Space: {vehicle?.data?.bootSpace}</p>
-              <p>Ground Clearance: {vehicle?.data?.groundClearance}</p>
-              <p>Cost Per Day: {vehicle?.data?.costPerDay}</p>
-              <p>Seat Count: {vehicle?.data?.seatCount}</p>
-              <p>Booking Status: {vehicle?.data?.bookingStatus}</p>
-              <p>Average Rating: {vehicle?.data?.averageRating}</p>
-              <p>Category: {vehicle?.data?.categories?.category}</p>
-              <p>Sub Category: {vehicle?.data?.categories?.subCategory}</p>
-              <p>Currrent Location: {vehicle?.data?.currrentLocation}</p>
+            <Col md={6}>
+              <h1>{data?.model}</h1>
+
+              <Card>
+                <Card.Body>
+                  <Card.Text>
+                    <p>Transmission: {data?.transmission}</p>
+                    <p>Fuel Type: {data?.fuelType}</p>
+                    <p>Engine: {data?.engine}</p>
+                    <p>Boot Space: {data?.bootSpace}</p>
+                    <p>Ground Clearance: {data?.groundClearance}</p>
+                    <p>Cost Per Day: {data?.costPerDay}</p>
+                    <p>Seat Count: {data?.seatCount}</p>
+                    <p>Booking Status: {data?.bookingStatus}</p>
+                    <p>Rating: {data?.averageRating}</p>
+                    <p>Category: {data?.categories?.category}</p>
+                    <p>Sub Category: {data?.categories?.subCategory}</p>
+                    <p>Currrent Location: {data?.currrentLocation}</p>
+                  </Card.Text>
+                </Card.Body>
+
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>
+                    <>
+                      <Card.Link href="#">Card Link</Card.Link>
+                      <Card.Link href="#">Another Link</Card.Link>
+                    </>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
             </Col>
           </Row>
         </>
