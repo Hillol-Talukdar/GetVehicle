8972c937234890e28bda2c25f65445ebf80bd99f
Hillol Talukdar <talukdarhillol@gmail.com>
2022-09-20 02:02:15 +0600

    Update UI of Vehicle details page

--- Website/client/src/Components/Item/VehicleDatalistItem.js
+++ Website/client/src/Components/Item/VehicleDatalistItem.js
@@ -8,48 +8,60 @@ const VehicleDatalistItem = ({ data }) => {
         Transmission:
         <span>{data?.transmission}</span>
       </ListGroup.Item>
+
       <ListGroup.Item className="d-flex justify-content-between">
-        Fuel Type: {data?.fuelType}
+        Fuel Type:
+        <span>{data?.fuelType}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Engine: {data?.engine}
+        Engine:
+        <span>{data?.engine}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Boot Space: {data?.bootSpace}
+        Boot Space:
+        <span>{data?.bootSpace}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Ground Clearance: {data?.groundClearance}
+        Ground Clearance:
+        <span>{data?.groundClearance}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Cost Per Day: {data?.costPerDay}
+        Cost Per Day:
+        <span>{data?.costPerDay}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Seat Count: {data?.seatCount}
+        Seat Count:
+        <span>{data?.seatCount}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Booking Status: {data?.bookingStatus}
+        Booking Status:
+        <span>{data?.bookingStatus}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Rating: {data?.averageRating}
+        Rating:
+        <span>{data?.averageRating}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Category: {data?.categories?.category}
+        Category:
+        <span>{data?.categories?.category}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Sub Category: {data?.categories?.subCategory}
+        Sub Category:
+        <span>{data?.categories?.subCategory}</span>
       </ListGroup.Item>
 
       <ListGroup.Item className="d-flex justify-content-between">
-        Currrent Location: {data?.currrentLocation}
+        Currrent Location:
+        <span>{data?.currrentLocation}</span>
       </ListGroup.Item>
     </ListGroup>
   );
