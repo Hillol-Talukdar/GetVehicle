b1e74d7119647dd680bdb67cec161cf3dbd76e59
Hillol Talukdar <talukdarhillol@gmail.com>
2022-10-18 02:28:01 +0600

    Create CreateOrUpdateCategoryForm and  make neccessary functions in CreateOrUpdateCategoryContainer

--- Website/client/src/Components/ContainerComponents/Admin/Item/CreateOrUpdateCategoryContainer/CreateOrUpdateCategoryContainer.js
+++ Website/client/src/Components/ContainerComponents/Admin/Item/CreateOrUpdateCategoryContainer/CreateOrUpdateCategoryContainer.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import {
+  createCategory,
+  getAllCategories,
+} from '../../../../../Services/CategoryDataService';
+import CreateOrUpdateCategoryForm from '../../../../Forms/CreateOrUpdateCategoryForm';
 
 const CreateOrUpdateCategoryContainer = () => {
-  return (
-    <div>CreateOrUpdateCategoryContainer</div>
-  )
-}
+  const user = useSelector((state) => state.userReducer);
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
 
-export default CreateOrUpdateCategoryContainer
+  useEffect(() => {
+    loadAllCategories();
+  }, []);
+
+  const loadAllCategories = () =>
+    getAllCategories().then((res) => setCategories(res.data.data));
+
+  const changeHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    createCategory(name, user.token)
+      .then((res) => {
+        setLoading(false);
+        toast.success(`"${res.data.data.name}" is created!`);
+        setName('');
+        loadAllCategories();
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast.error(
+          err.response && err.response.data.message
+            ? err.response.data.message
+            : err.message
+        );
+      });
+  };
+
+  return (
+    <Container fluid>
+      <div className="d-flex justify-content-between border-bottom mb-3 border-2">
+        {loading ? (
+          <h4 className="ml-auto">Createing Category...</h4>
+        ) : (
+          <h4 className="ml-auto">Create New Category</h4>
+        )}
+      </div>
+
+      <CreateOrUpdateCategoryForm
+        name={name}
+        submitHandler={submitHandler}
+        changeHandler={changeHandler}
+        buttonName="Create"
+      />
+    </Container>
+  );
+};
+
+export default CreateOrUpdateCategoryContainer;
---
+++ Website/client/src/Components/Forms/CreateOrUpdateCategoryForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { VehicleInfoConstants } from '../../Constants/CommonConstants';
+
+const CreateOrUpdateCategoryForm = ({
+  name,
+  submitHandler,
+  changeHandler,
+  buttonName,
+}) => {
+  return (
+    <Form onSubmit={submitHandler}>
+      <Form.Group
+        className="form-group d-flex align-items-center col-sm-4 flex-column mx-auto mb-3"
+        controlId="formBasicCategoryName"
+      >
+        <Form.Label>
+          {VehicleInfoConstants.CATEGORY}
+          <span className="text-danger"> *</span>
+        </Form.Label>
+
+        <Form.Control
+          type="Text"
+          name="name"
+          placeholder={VehicleInfoConstants.CATEGORY}
+          value={name}
+          onChange={changeHandler}
+          required
+        />
+      </Form.Group>
+
+      <Button
+        className="mx-auto mt-3 d-grid gap-2 col-sm-4 mb-3"
+        variant="primary"
+        type="submit"
+      >
+        {buttonName}
+      </Button>
+    </Form>
+  );
+};
+
+export default CreateOrUpdateCategoryForm;
