9989f5146d9f68b12f4e1177b9c96aa932967ad2
DESKTOP-2FO73RC\USER <tahminkabir13@gmail.com>
2022-12-03 15:39:41 +0600

    #73: Load single vehicle details from Server

--- Android App/app/src/main/java/com/raiyan_hillol/getvehicle/data/usecase/VehicleUseCase.java
+++ Android App/app/src/main/java/com/raiyan_hillol/getvehicle/data/usecase/VehicleUseCase.java
@@ -63,67 +63,28 @@ public class VehicleUseCase {
         return sanitizedPhotos;
     }
 
-    //TODO: make the response things reusable
-    public static void getSingleVehicle(String vehicleId, Context context) {
-
-        final VehicleData[] vehicleData = new VehicleData[1];
-
-        RequestQueue requestQueue;
-
-        Cache cache = new DiskBasedCache(context.getCacheDir(), 1024 * 1024); // 1MB cap
 
 
-        Network network = new BasicNetwork(new HurlStack());
-
-
-        requestQueue = new RequestQueue(cache, network);
-
-
-        requestQueue.start();
-
-        String url = AppUriConstants.GET_ALL_VEHICLE_URI + vehicleId;
-//        String url = "http://192.168.0.9:4000/api/vehicle/" + vehicleId;
-
-        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                vehicleData[0] = getVehicleFromJSONObject(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                getSingleVehicle(vehicleId, context);
-                error.printStackTrace();
-            }
-        });
-        requestQueue.add(jsonArrayRequest);
-
-    }
-
-    private static VehicleData getVehicleFromJSONObject(JSONObject response) {
-        VehicleData vehicleData = null;
+    public static VehicleData getVehicleDataFromJSONObject(JSONObject vehicleObject) {
+        VehicleData vehicleData = new VehicleData();
         try {
-            JSONObject jsonObject = response.getJSONObject("data");
-            vehicleData = new VehicleData(
-                    jsonObject.getString("_id"),
-                    jsonObject.getString("model"),
-                    jsonObject.getString("vehicleType"),
-//                    jsonObject.getString("genericType"),
-                    jsonObject.getString("transmission"),
-                    jsonObject.getString("fuelType"),
-                    jsonObject.getString("engine"),
-//                    jsonObject.getString("bootSpace"),
-//                    jsonObject.getString("groundClearance"),
-                    jsonObject.getInt("costPerDay"),
-//                    jsonObject.getInt("seatCount"),
-//                    jsonObject.getDouble("Mileage"),
-//                    jsonObject.getDouble("averageRating"),
-                    jsonObject.getJSONObject("currentLocation").getString("address")
-//                    jsonObject.getBoolean("bookingStatus"),
-//                    Tools.getStringArrayListFromJSONArray(jsonObject.getJSONArray("photos")),
-//                    jsonObject.getString("user")
-            );
+            vehicleData.setId(vehicleObject.getString("_id"));
+            vehicleData.setModel(vehicleObject.getString("model"));
+            vehicleData.setCategory(vehicleObject.getJSONObject("category"));
+            vehicleData.setSubCategory(vehicleObject.getJSONObject("subCategory"));
+            vehicleData.setTransmission(vehicleObject.getString("transmission"));
+            vehicleData.setFuelType(vehicleObject.getString("fuelType"));
+            vehicleData.setEngine(vehicleObject.getString("engine"));
+            vehicleData.setBootSpace(vehicleObject.getString("bootSpace"));
+            vehicleData.setGroundClearance(vehicleObject.getString("groundClearance"));
+            vehicleData.setCostPerDay(vehicleObject.getInt("costPerDay"));
+            vehicleData.setSeatCount(vehicleObject.getInt("seatCount"));
+            vehicleData.setMileage(vehicleObject.getInt("mileage"));
+            vehicleData.setAverageRating(vehicleObject.getDouble("averageRating"));
+            vehicleData.setCurrentLocationString(vehicleObject.getString("currentLocationString"));
+            vehicleData.setBookingStatus(vehicleObject.getBoolean("bookingStatus"));
+            vehicleData.setPhoto(VehicleUseCase.getSanitizedPhotos(vehicleObject.getJSONArray("photo"), vehicleData.getCategory()));
+            vehicleData.setTrashed(vehicleObject.getBoolean("isTrashed"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -135,28 +96,7 @@ public class VehicleUseCase {
         try {
             JSONArray responseData = jsonObject.getJSONArray("data");
             for (int i = 0; i < responseData.length(); i++) {
-                JSONObject vehicleObject = responseData.getJSONObject(i);
-
-                VehicleData vehicleData = new VehicleData();
-                vehicleData.setId(vehicleObject.getString("_id"));
-                vehicleData.setModel(vehicleObject.getString("model"));
-                vehicleData.setCategory(vehicleObject.getJSONObject("category"));
-                vehicleData.setSubCategory(vehicleObject.getJSONObject("subCategory"));
-                vehicleData.setTransmission(vehicleObject.getString("transmission"));
-                vehicleData.setFuelType(vehicleObject.getString("fuelType"));
-                vehicleData.setEngine(vehicleObject.getString("engine"));
-                vehicleData.setBootSpace(vehicleObject.getString("bootSpace"));
-                vehicleData.setGroundClearance(vehicleObject.getString("groundClearance"));
-                vehicleData.setCostPerDay(vehicleObject.getInt("costPerDay"));
-                vehicleData.setSeatCount(vehicleObject.getInt("seatCount"));
-                vehicleData.setMileage(vehicleObject.getInt("mileage"));
-                vehicleData.setAverageRating(vehicleObject.getDouble("averageRating"));
-                vehicleData.setCurrentLocationString(vehicleObject.getString("currentLocationString"));
-                vehicleData.setBookingStatus(vehicleObject.getBoolean("bookingStatus"));
-                vehicleData.setPhoto(VehicleUseCase.getSanitizedPhotos(vehicleObject.getJSONArray("photo"), vehicleData.getCategory()));
-                vehicleData.setTrashed(vehicleObject.getBoolean("isTrashed"));
-
-                allVehicleData.add(vehicleData);
+                allVehicleData.add(getVehicleDataFromJSONObject(responseData.getJSONObject(i)));
             }
         } catch (JSONException e) {
             e.printStackTrace();
--- Android App/app/src/main/java/com/raiyan_hillol/getvehicle/screens/VehicleDetailsScreen/view/VehicleDetailsActivity.java
+++ Android App/app/src/main/java/com/raiyan_hillol/getvehicle/screens/VehicleDetailsScreen/view/VehicleDetailsActivity.java
@@ -2,17 +2,35 @@ package com.raiyan_hillol.getvehicle.screens.VehicleDetailsScreen.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.raiyan_hillol.getvehicle.R;
+import com.raiyan_hillol.getvehicle.constants.AppUriConstants;
+import com.raiyan_hillol.getvehicle.data.model.VehicleData;
 import com.raiyan_hillol.getvehicle.data.usecase.VehicleUseCase;
 import com.raiyan_hillol.getvehicle.databinding.ActivityVehicleDetailsBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class VehicleDetailsActivity extends AppCompatActivity {
 
     private ActivityVehicleDetailsBinding binding;
+    private static VehicleData currentSelectedVehicleData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +41,23 @@ public class VehicleDetailsActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-
-        loadVehicleDetails();
+        if(currentSelectedVehicleData == null) {
+            getCurrentVehicleDetails();
+        }
         initWedgies();
     }
 
-    private void loadVehicleDetails() {
+    private void getCurrentVehicleDetails() {
         Intent intent = getIntent();
         String vehicleId = intent.getStringExtra("selected_vehicle_id");
+        getSingleVehicle(vehicleId, getApplicationContext());
+    }
+
+    private void setVehicleDetailsToView() {
+
 //        VehicleData singleVehicleData = Tools.getSingleVehicle(vehicleId, getApplicationContext());
-
-        VehicleUseCase.getSingleVehicle(vehicleId, getApplicationContext());
-
-//        TextView vehicleModel = findViewById(R.id.vehicle_model_detail);
+        TextView vehicleModel = findViewById(R.id.tvModel);
+        vehicleModel.setText(currentSelectedVehicleData.getModel());
 //        TextView vehicleType = findViewById(R.id.vehicle_type_detail);
 //        TextView vehicleTransmission = findViewById(R.id.vehicle_transmission_detail);
 //        TextView vehicleCurrentLocation = findViewById(R.id.current_location_detail);
@@ -48,5 +70,37 @@ public class VehicleDetailsActivity extends AppCompatActivity {
 
     private void initWedgies() {
 //        binding.tvModel =
+    }
+
+    public void getSingleVehicle(String vehicleId, Context context) {
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(context.getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+
+        requestQueue.start();
+
+        String url = AppUriConstants.GET_ALL_VEHICLE_URI + vehicleId;
+//        String url = "http://192.168.0.9:4000/api/vehicle/" + vehicleId;
+
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    currentSelectedVehicleData = VehicleUseCase.getVehicleDataFromJSONObject(response.getJSONObject("data"));
+                    setVehicleDetailsToView();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                getSingleVehicle(vehicleId, context);
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
     }
 }
