2cf6e756a0f09ae1a0b9115fed22c50762dc31aa
DESKTOP-2FO73RC\USER <tahminkabir13@gmail.com>
2022-12-04 01:15:08 +0600

    #79: Add new elements and icons on booking page

--- Website/client/src/Components/ContainerComponents/Admin/Booking/Booking.js
+++ Website/client/src/Components/ContainerComponents/Admin/Booking/Booking.js
@@ -6,14 +6,15 @@ import { googleLogin } from '../../../../Services/GoogleAuthService';
 import { getVehicleDetails } from '../../../../Services/VehicleDataService';
 import AskForLoginModal from '../../../Modal/AskForLoginModal';
 import { ImLocation } from 'react-icons/im';
-import { TbCurrencyTaka } from 'react-icons/tb';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { FcMoneyTransfer } from 'react-icons/fc';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './Booking.css';
 
 const Booking = () => {
   const user = useSelector((state) => state.userReducer);
   const [showLoginModal, setShowLoginModal] = useState(user ? false : true);
+  const [phoneNumber, setPhoneNumber] = useState('+880 ');
 
   const { id } = useParams();
 
@@ -43,6 +44,12 @@ const Booking = () => {
     handleLoginModalClose();
   };
 
+  const checkAndSetPhoneNumber = (e) => {
+    let phoneNumberInput = e.target.value;
+    if (phoneNumberInput.length <= 4) return;
+    setPhoneNumber(e.target.value);
+  };
+
   return (
     <Container fluid>
       {loading ? (
@@ -51,55 +58,86 @@ const Booking = () => {
         <p>{error}</p>
       ) : (
         <>
-          <div className='booking-container'>
-            <div className='left-container'>
-            {user && (
-              <div className='user-details enhance-div'>
-                <span className='field-bottom-margin'>Delivery To: {user.name}</span>
-                <span>Email: {user.email}</span>
-              </div>
-              )
-            }
-
-            {vehicleData && (
-              <div className='vehicle-details enhance-div'>
-                <div className='vehicle-image'>
-                <Image
-                    id='vehicle-image'
-                    src={vehicleData.photo.length ? vehicleData.photo[0] : 
-                      vehicleData?.category?.name === 'Bike' ? '/templateBike.jpg' : '/templateCar.jpg'
-                    }
-                    alt='Vehicle Image'
-                    key='vehicle-image'
-                    fluid
-                  />
-                </div>
-                <div className='vehicle-info'>
-                  <span className='vehicle-model'>{vehicleData.model}</span>
-                  <span className='field-bottom-margin vehicle-category'>{vehicleData.subCategory.name} {vehicleData.category.name}</span>
-                  {/* <span className='field-bottom-margin location-details enhance-div'><TbCurrencyTaka/> Cost Per Day: {vehicleData.costPerDay}</span> */}
-                  <div className='location-details enhance-div'>
-                    <span>You will get your delivery from <ImLocation/> {vehicleData.currentLocationString}</span>  
+          <div className="booking-container">
+            <div className="left-container">
+              {user && (
+                <div className="user-details enhance-div">
+                  <div className="field-bottom-margin-x-lg">
+                    <span className="enhanced-label">Delivery To</span>
+                    <span>{user.name}</span>
+                  </div>
+                  <div className="email">
+                    <span className="enhanced-label">Email</span>
+                    <span>{user.email}</span>
+                  </div>
+                  <div className="phone-number">
+                    <span className="phone-number-label enhanced-label">
+                      Phone
+                    </span>
+                    <input
+                      className="form-control phone-number-input form-control-sm"
+                      required
+                      value={phoneNumber}
+                      onChange={checkAndSetPhoneNumber}
+                    ></input>
+                    <span className="text-danger required-text">Required</span>
                   </div>
                 </div>
-              </div>
-            )
-            }
+              )}
+
+              {vehicleData && (
+                <div className="vehicle-details enhance-div">
+                  <div className="vehicle-image enhance-div">
+                    <Image
+                      id="vehicle-image"
+                      src={
+                        vehicleData.photo.length
+                          ? vehicleData.photo[0]
+                          : vehicleData?.category?.name === 'Bike'
+                          ? '/templateBike.jpg'
+                          : '/templateCar.jpg'
+                      }
+                      alt="Vehicle Image"
+                      key="vehicle-image"
+                      fluid
+                    />
+                  </div>
+                  <div className="vehicle-info">
+                    <div className="vehicle-info-title enhance-div vehicle-info-items">
+                      <span className="vehicle-model">{vehicleData.model}</span>
+                      <span className="field-bottom-margin vehicle-category">
+                        {vehicleData.subCategory.name}{' '}
+                        {vehicleData.category.name}
+                      </span>
+                    </div>
+                    <span className="field-bottom-margin vehicle-info-items enhance-div">
+                      <img src="/money-icon.png" /> Cost Per Day:{' '}
+                      {vehicleData.costPerDay}
+                    </span>
+                    <div className="vehicle-info-items enhance-div">
+                      <span>
+                        You can pick up your vehicle from{' '}
+                        <img src="/location-icon.png" />{' '}
+                        {vehicleData.currentLocationString}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-            
-            <div className='booking-info enhance-div'>
-              Select the range when you want to book
-              <div className='date-picker-container'>
+
+            <div className="booking-info enhance-div">
+              <div className="date-picker-container">
+                <span className="field-bottom-margin">Select Date Range</span>
                 <DatePicker
-                selected={startDate}
-                onChange={onChange}
-                startDate={startDate}
-                endDate={endDate}
-                selectsRange
-                inline
-              />
+                  selected={startDate}
+                  onChange={onChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  selectsRange
+                  inline
+                />
               </div>
-            
             </div>
           </div>
           {/* <AskForLoginModal
