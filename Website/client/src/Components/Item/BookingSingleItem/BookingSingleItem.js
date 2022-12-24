923a07a82fd81f5c3d98b7f34fc49a4eff8dafdb
DESKTOP-2FO73RC\USER <tahminkabir13@gmail.com>
2022-12-25 02:06:41 +0600

    #82: Design booking list page

--- Website/client/src/Components/Item/BookingSingleItem/BookingSingleItem.js
+++ Website/client/src/Components/Item/BookingSingleItem/BookingSingleItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import './BookingSingleItem.css';
 
 const BookingSingleItem = (props) => {
@@ -11,48 +11,115 @@ const BookingSingleItem = (props) => {
 
   return (
     <div className="booking-single-item-container d-flex justify-content-between">
-      <div className='w-100'>
-        <span>{currentItem?.vehicle?.model} ({currentItem?.vehicle?._id})</span>
+      <div className="w-100 d-flex mb-4">
+        <div>
+          <span className="enhanced-label">
+            {currentItem?.vehicle?.model} ({currentItem?.vehicle?._id})
+          </span>
+        </div>
+
+        <div style={{ marginLeft: 'auto' }}>
+          <Button
+            size="sm"
+            style={{ fontSize: 'medium', marginRight: '10px' }}
+            variant="outline-primary"
+          >
+            User Details
+          </Button>
+          <Button
+            size="sm"
+            style={{ fontSize: 'medium' }}
+            variant="outline-primary"
+          >
+            Save Changes
+          </Button>
+        </div>
       </div>
+
       <div>
         <span>Booked For: </span>
-        <span>{currentItem?.totalDays} </span>
-        <span>Days</span>
+        <span className="enhanced-label">
+          {currentItem?.totalDays} {currentItem?.totalDays > 1 ? 'Days' : 'Day'}
+        </span>
       </div>
+
       <div>
         <span>Total Amount: </span>
-        <span>{currentItem?.totalAmount} </span>
-        <span>Taka</span>
+        <span className="enhanced-label">{currentItem?.totalAmount} Taka</span>
       </div>
+
       <div>
         <span>Payment Method: </span>
-        <span>{currentItem?.paymentMethod} </span>
+        <span className="enhanced-label">{currentItem?.paymentMethod} </span>
       </div>
+
       <div>
-        <span>Payment Status: </span>
-        <span>{currentItem?.paid ? 'Paid' : 'Unpaid'}</span>
+        <span>Booked From: </span>
+        <span className="enhanced-label">
+          {getFormattedDate(currentItem?.handOverDate)}
+        </span>
       </div>
+
       <div>
-        <span>Handed Over To User: </span>
-        <span>{currentItem?.handedOver ? 'Yes' : 'No'}</span>
+        <span>Booked Till: </span>
+        <span className="enhanced-label">
+          {getFormattedDate(currentItem?.receiveDate)}
+        </span>
       </div>
-      <div>
-        <span>Hand Over To User On: </span>
-        <span>{getFormattedDate(currentItem?.handOverDate)}</span>
+
+      <div className="d-flex">
+        <span className="dropdown-label">Payment Status: </span>
+        <div>
+          <Form.Select
+            className="enhanced-select"
+            name="payment_status"
+            size="sm"
+          >
+            <option value="true" selected={currentItem?.paid === true}>
+              Paid
+            </option>
+            <option value="false" selected={currentItem?.paid === false}>
+              Unpaid
+            </option>
+          </Form.Select>
+        </div>
       </div>
-      <div>
-        <span>Got Back From User: </span>
-        <span>{currentItem?.received ? 'Yes' : 'No'}</span>
+
+      <div className="d-flex">
+        <span className="dropdown-label">Handed Over To User: </span>
+        <div>
+          <Form.Select
+            className="enhanced-select"
+            name="is_handed_over_to_user"
+            size="sm"
+          >
+            <option value="true" selected={currentItem?.handedOver === true}>
+              Yes
+            </option>
+            <option value="false" selected={currentItem?.handedOver === false}>
+              No
+            </option>
+          </Form.Select>
+        </div>
       </div>
-      <div>
-        <span>Will Get Back From User On: </span>
-        <span>{getFormattedDate(currentItem?.receiveDate)}</span>
+
+      <div className="d-flex">
+        <span className="dropdown-label">Got Back From User: </span>
+        <div>
+          <Form.Select
+            className="enhanced-select"
+            name="is_handed_over_to_user"
+            size="sm"
+          >
+            <option value="true" selected={currentItem?.received === true}>
+              Yes
+            </option>
+            <option value="false" selected={currentItem?.received === false}>
+              No
+            </option>
+          </Form.Select>
+        </div>
       </div>
-     <div style={{marginLeft: 'auto'}}>
-        <Button size='sm' variant="outline-dark">User Details</Button>
-     </div>
-    
-      
     </div>
   );
 };
