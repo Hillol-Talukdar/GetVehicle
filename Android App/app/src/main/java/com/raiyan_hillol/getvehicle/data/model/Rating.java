883d50187550813fe7078d1aa83e67b0f85497aa
DESKTOP-2FO73RC\USER <tahminkabir13@gmail.com>
2023-01-20 14:46:52 +0600

    #92: Add unstaged important files

---
+++ Android App/app/src/main/java/com/raiyan_hillol/getvehicle/data/model/Rating.java
@@ -0,0 +1,31 @@
+package com.raiyan_hillol.getvehicle.data.model;
+
+public class Rating {
+    private String id;
+    private int star;
+    private String postedBy;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
+}
